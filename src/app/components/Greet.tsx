208dcc728fdf3a3473f0756247c163147dd048c6
Dev AKS <devaks24k@gmail.com>
2025-02-03 23:03:12 -0500

    Add greet component

---
+++ src/app/components/Greet.tsx
@@ -0,0 +1,8 @@
+export const Greet = () => {
+    console.log("Greet Component!");
+    return (
+        <div className="mb-4">
+            <h1 className="text-2xl font-bold"> Greet Component!</h1>
+        </div>
+    );
+};
